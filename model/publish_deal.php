28dac63d94599935a5e9e16d83137ed74160c0c7
itxiaolong3 <1710600973@qq.com>
2017-12-19 14:28:05 +0800

    在发帖时出现异常能原值返回输入框中的值

--- model/publish_deal.php
+++ model/publish_deal.php
@@ -14,16 +14,23 @@ $mysql=MySqlDBUtil::getInstance($arr);
 $pub_title=escapeString($_POST['pub_title']);
 $pub_content=escapeString($_POST['pub_content']);
 $pub_module_id=escapeString($_POST['module_id']);
+session_start();
 //var_dump("分类标志"+$_POST['module_id']) ;
 //判断数据的合法性
 if (empty($pub_module_id)){
+    //保存标题
+    $_SESSION['inputTitle']=$pub_title;
+    //保存内容
+    $_SESSION['inputContent']=$pub_content;
     jump('./publish.php',2,'请选择帖子类型');
 }else if (empty($pub_title)||empty($pub_content)){
+    //保存内容
+    $_SESSION['inputContent']=$pub_content;
     jump('./publish.php',2,'标题或内容不可为空');
 }
 //把所有发帖数据写入数据库中
 //$pub_owner='游客';//应该是博主
-session_start();
+
 if (isset($_SESSION['userinfo'])){
     $pub_owner=$_SESSION['userinfo']['user_name'];//应该是博主
 }else{
@@ -33,6 +40,8 @@ $pub_time=time();
 $sql="insert into publish values(null,'$pub_title','$pub_content','$pub_owner','$pub_time',DEFAULT ,DEFAULT,'$pub_module_id' )";
 $result=$mysql->my_query($sql);
 if ($result){
+    $_SESSION['inputContent']=array();
+    $_SESSION['inputTitle']=array();
     //发表成功
     jump('./list_father.php');
 
