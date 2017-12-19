9a73868e8ae5c7f9f296684234252f22b321e978
itxiaolong3 <1710600973@qq.com>
2017-12-19 12:54:42 +0800

    发表贴文加上分类标志

--- model/publish_deal.php
+++ model/publish_deal.php
@@ -13,9 +13,12 @@ $mysql=MySqlDBUtil::getInstance($arr);
 //获取传来的数据
 $pub_title=escapeString($_POST['pub_title']);
 $pub_content=escapeString($_POST['pub_content']);
-
+$pub_module_id=escapeString($_POST['module_id']);
+//var_dump("分类标志"+$_POST['module_id']) ;
 //判断数据的合法性
-if (empty($pub_title)||empty($pub_content)){
+if (empty($pub_module_id)){
+    jump('./publish.php',2,'请选择帖子类型');
+}else if (empty($pub_title)||empty($pub_content)){
     jump('./publish.php',2,'标题或内容不可为空');
 }
 //把所有发帖数据写入数据库中
@@ -27,7 +30,7 @@ if (isset($_SESSION['userinfo'])){
     $pub_owner='非法人员';
 }
 $pub_time=time();
-$sql="insert into publish values(null,'$pub_title','$pub_content','$pub_owner','$pub_time',DEFAULT ,DEFAULT )";
+$sql="insert into publish values(null,'$pub_title','$pub_content','$pub_owner','$pub_time',DEFAULT ,DEFAULT,'$pub_module_id' )";
 $result=$mysql->my_query($sql);
 if ($result){
     //发表成功
